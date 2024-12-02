15fe640190ebc60f5f4440270fb51e8eeeca87cd
borisGansGit <bgans.ieu2023@student.ie.edu>
2024-12-02 08:32:48 +0100

    --

---
+++ src/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client.dir/client.c.o"
+  "CMakeFiles/Client.dir/client.c.o.d"
+  "CMakeFiles/Client.dir/network_utils.c.o"
+  "CMakeFiles/Client.dir/network_utils.c.o.d"
+  "bin/Client"
+  "bin/Client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/chat_room.c.o"
+  "CMakeFiles/Server.dir/chat_room.c.o.d"
+  "CMakeFiles/Server.dir/network_utils.c.o"
+  "CMakeFiles/Server.dir/network_utils.c.o.d"
+  "CMakeFiles/Server.dir/server.c.o"
+  "CMakeFiles/Server.dir/server.c.o.d"
+  "bin/Server"
+  "bin/Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
